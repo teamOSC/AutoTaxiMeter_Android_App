67f56e8242170d279397b948c90edb356b8d9980
Arnav Gupta <championswimmer@aokp.co>
2013-12-23 06:44:03 +0530

    add Fare utils   this class will contain the json parser too   as well as the fare calculation algorithms

---
+++ App/src/main/java/in/ac/dtu/autotaximeter/Utils/Fare.java
@@ -0,0 +1,13 @@
+package in.ac.dtu.autotaximeter.Utils;
+
+/**
+ * Created by championswimmer on 23/12/13.
+ */
+public class Fare {
+    public String city = "Delhi";
+    public String operator = "Auto";
+    public Double farePerKm = 8.0;
+    public Double fixedFare = 25.0;
+    public Integer fixedKm = 2;
+    public Double bookingFee = 0.0;
+}
